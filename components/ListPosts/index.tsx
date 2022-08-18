3f2f1b3e5a1db8904fa8b8ff58f383d554f8b54f
brunoleo223 <brunoleo223@gmail.com>
2022-08-17 23:12:41 -0300

    promo aulas ao vivo

--- components/ListPosts/index.tsx
+++ components/ListPosts/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { IPost } from "../../interfaces/Interfaces";
 import Post from "../Post";
 import Promo from "../Promo";
@@ -25,7 +26,13 @@ export const ListPosts = (data: ListPostsProps) => {
                             <>
                             {(count === 1 || count == 4) &&
                                 <Promo>
-                                    <h2>{post.title}</h2>
+                                    <div className="conteudo">
+                                        <h2>Wordpress na prática, sem enrolação</h2>
+                                        <p>Estude com aulas ao vivo toda semana</p>
+                                        <Link href="https://codeskills.com.br/lab/curso-de-wordpress/">
+                                            <a  target="_blank" className="btn">Saiba mais</a>
+                                        </Link>
+                                    </div>
                                 </Promo>
                             }
                             <Post key={post.id} data={post} />
